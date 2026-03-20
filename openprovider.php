33c85bd1092e2e2ca9161816acd3de260ec452fc
Abdy Franco <abdy@devmail.one>
2026-03-20 16:40:01 -0600

    Replaced serialize/unserialize with safer versions

--- openprovider.php
+++ openprovider.php
@@ -2048,7 +2048,7 @@ class Openprovider extends RegistrarModule
         );
 
         if ($cache) {
-            $result = unserialize(base64_decode($cache));
+            $result = safe_unserialize(base64_decode($cache));
         }
 
         Loader::loadModels($this, ['Currencies']);
@@ -2072,7 +2072,7 @@ class Openprovider extends RegistrarModule
                 try {
                     Cache::writeCache(
                         'tlds_prices',
-                        base64_encode(serialize($result)),
+                        base64_encode(safe_serialize($result)),
                         strtotime(Configure::get('Blesta.cache_length')) - time(),
                         Configure::get('Blesta.company_id') . DS . 'modules' . DS . 'openprovider' . DS
                     );
