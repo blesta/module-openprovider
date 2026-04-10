6a8497792d01c1cc4542640aa875cbceac0e01af
Jonathan Reissmueller <jonathan@phillipsdata.com>
2026-04-10 10:51:59 -0400

    CORE-5741: Revert safe_serialize to serialize

--- openprovider.php
+++ openprovider.php
@@ -2072,7 +2072,7 @@ class Openprovider extends RegistrarModule
                 try {
                     Cache::writeCache(
                         'tlds_prices',
-                        base64_encode(safe_serialize($result)),
+                        base64_encode(serialize($result)),
                         strtotime(Configure::get('Blesta.cache_length')) - time(),
                         Configure::get('Blesta.company_id') . DS . 'modules' . DS . 'openprovider' . DS
                     );
